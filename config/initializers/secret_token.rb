318cde0d7b851e77b636ca3800500119b1eb04ea
hayksaakian <fakest.id.ever@gmail.com>
2012-10-23 20:48:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gameballs::Application.config.secret_token = '5beb6f033d549f68040ece50f0e781f454ff681a2bfa62f36e56e628e834bdba403fc092e390f1d870e0c0fa07eafc711f2439968dcceb8c8e975741c58ac84b'
