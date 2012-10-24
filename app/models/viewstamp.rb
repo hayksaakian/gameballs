db34bf1eb6cfbe023ae807fbe99247d6d1537488
hayksaakian <fakest.id.ever@gmail.com>
2012-10-23 23:32:03 -0700

    ui

--- app/models/viewstamp.rb
+++ app/models/viewstamp.rb
@@ -3,6 +3,7 @@ class Viewstamp
   include Mongoid::Timestamps
   belongs_to :game
   field :viewers, :type => Integer
+  field :channels, :type => Integer
   field :twitch_viewers, :type => Integer
   field :twitch_channels, :type => Integer
   field :owned_viewers, :type => Integer
