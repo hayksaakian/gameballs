75d892d2fd9d27eec399290b78c7d4e1f4757aa7
hayksaakian <fakest.id.ever@gmail.com>
2012-11-02 22:41:42 -0700

    tihngsn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -27,7 +27,7 @@ class GamesController < ApplicationController
   # GET /games.json
   def index
     most_recent = Game.max(:last_update)
-    @games = Game.where(:last_update.gte => 1.hour.ago)
+    @games = Game.where(:last_update.gte => 3.hours.ago)
     @games = @games.order_by([:current_viewers , :desc]).limit(25)
 
     respond_to do |format|
